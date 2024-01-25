8e767d91d6a693a5947d1505118b653275c5b27b
atakee72 <atakee@gmail.com>
2024-01-26 00:32:00 +0100

    solved: the problem with the size of the pictures, when hovered over

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,6 +6,7 @@ import {
   syntax,
 } from "../../public/assets/index.js";
 import Image from "next/image.js";
+import Link from "next/link";
 
 const Header = () => (
   <section className="  w-full min-h-screen flex flex-col items-center justify-items-start mt-28">
@@ -21,78 +22,96 @@ const Header = () => (
         style={{ boxShadow: "-100px 10px 80px 20px #080707 inset" }}
       ></div>
 
-      <div className="mt-5 p-3 bg-primary gap-3 flex font-semibold text-xl w-full h-96 rounded-lg">
-        <div className="relative w-1/3 transition-all duration-500 hover:w-2/3">
+      <div className="mt-5 p-6 bg-primary gap-5 flex font-semibold text-xl w-[1600px] h-[550px] rounded-sm">
+        <Link
+          href="https://mahalle-kiez-gesichterbuch.vercel.app"
+          target="_blank"
+          className="relative w-1/3 transition-all duration-500 hover:w-2/3"
+        >
           <Image
-            className="h-full w-full aspect-square group-hover:aspect-[3/2]"
-            object-cover
+            className="h-full w-full rounded-sm object-cover overflow-hidden aspect-auto"
             src={maHalle.src}
-            height={600}
-            width={300}
+            height={400}
+            width={600}
           />
-          <div className="absolute leading-7 box-border m-0 flex flex-col items-start justify-center text-primary text-4xl top-0 left-0 z-20  w-full h-full bg-secondary/50 uppercase">
+          <div className="absolute leading-9 box-border m-0 flex flex-col items-start justify-center text-primary text-5xl top-0 left-0 z-20 w-full h-full bg-secondary/80 uppercase">
             {" "}
             <h1>A Home~made </h1>
             <h1>Facebook</h1>
             <h1>for my</h1>
             <h1>Neighbours</h1>
           </div>{" "}
-          <span className="absolute bg-secondary text-primary py-0.25 pl-1 pr-2 mb-1 ml-1 z-40 rounded-e-lg bottom-0 text-xs">
-            Next.js
-          </span>
-          <span className="absolute bg-secondary text-primary py-0.25 pl-1 pr-2 mb-1 ml-1 z-40 rounded-e-lg bottom-0 left-16 text-xs">
-            MongoDB
-          </span>
-          <span className="absolute bg-secondary text-primary py-0.25 pl-1 pr-2 mb-1 ml-1 z-40 rounded-e-lg bottom-0 left-36 text-xs">
-            NextAuth
-          </span>
-        </div>{" "}
-        <div className="relative w-1/3 transition-all duration-500 hover:w-2/3">
+          <div className="techBadges absolute bottom-1 z-40 text-primary text-xs">
+            <span className="bg-secondary/70 py-[1px] pl-1 pr-2 mb-1 ml-1 z-40 rounded-s-sm rounded-e-lg text-xs">
+              Next.js
+            </span>
+            <span className="bg-secondary/70 py-[1px] pl-1 pr-2 mb-1 ml-1 z-40 rounded-s-sm rounded-e-lg">
+              MongoDB
+            </span>
+            <span className="bg-secondary/70 py-[1px] pl-1 pr-2 mb-1 ml-1 z-40 rounded-s-sm rounded-e-lg">
+              NextAuth
+            </span>
+            <span className="bg-secondary/70 py-[1px] pl-1 pr-2 ml-1 mb-1 z-40 rounded-s-sm rounded-e-lg">
+              Tailwind
+            </span>
+          </div>
+        </Link>{" "}
+        <Link
+          href="https://dogs-n-films-catalog-app.vercel.app"
+          target="_blank"
+          className="relative w-1/3 transition-all duration-500 hover:w-2/3"
+        >
           <Image
-            className="h-full w-full aspect-square group-hover:aspect-[3/2]"
-            object-cover
+            className="h-full w-full rounded-sm object-cover overflow-hidden aspect-auto"
             src={graduate.src}
-            height={450}
-            width={300}
+            height={400}
+            width={600}
           />
-          <div className="absolute leading-7 box-border m-0 flex flex-col items-start justify-center text-primary text-4xl top-0 left-0 z-20 w-full h-full bg-secondary/50 uppercase">
+          <div className="absolute leading-9 box-border m-0 flex flex-col items-start justify-center text-primary text-5xl top-0 left-0 z-20 w-full h-full bg-secondary/80 uppercase">
             <h1>Can </h1>
             <h1>Dogs</h1>
-            <h1 className="absolute left-40"> & </h1>
+            <h1 className="absolute left-56"> & </h1>
             <h1>Movies</h1>
             <h1>go together</h1>
-            <span className="absolute top-[228px] left-64 rotate-180">?</span>
+            <span className="absolute top-[315px] left-[340px] rotate-180">
+              ?
+            </span>
           </div>
-          <span className="absolute bg-secondary text-primary py-0.25 pl-1 pr-2 ml-1 mb-1 z-40 rounded-e-lg bottom-0 text-xs">
-            Next.js
-          </span>
-          <span className="absolute bg-secondary text-primary py-0.25 pl-1 pr-2 ml-1 mb-1 z-40 rounded-e-lg bottom-0 left-16 text-xs">
-            Tailwind
-          </span>
-        </div>
-        <div className="relative group text-secondary w-1/3 transition-all duration-500 hover:w-2/3">
+          <div className="techBadges absolute bottom-1 z-40 text-primary text-xs">
+            <span className="bg-secondary/70 py-[1px] pl-1 pr-2 mb-1 ml-1 z-40 rounded-s-sm rounded-e-lg text-xs">
+              Next.js
+            </span>
+
+            <span className="bg-secondary/70 py-[1px] pl-1 pr-2 ml-1 mb-1 z-40 rounded-s-sm rounded-e-lg">
+              Tailwind
+            </span>
+          </div>
+        </Link>
+        <div className="relative w-1/3 transition-all duration-500 hover:w-2/3">
           <Image
-            className="h-full w-full aspect-square group-hover:aspect-[3/2]"
-            object-cover
+            className="h-full w-full rounded-sm object-cover overflow-hidden aspect-auto"
             src={dashboard.src}
-            height={450}
-            width={300}
+            height={400}
+            width={600}
           />
-          <div className="absolute leading-7 box-border m-0 flex flex-col items-start justify-center text-primary text-4xl top-0 left-0 z-20  w-full h-full bg-secondary/50 uppercase">
+          <div className="absolute leading-9 box-border m-0 flex flex-col items-start justify-center text-primary text-5xl top-0 left-0 z-20 w-full h-full bg-secondary/80 uppercase">
             {" "}
             <h1>Yet</h1> <h1>another </h1>
             <h1p>admin</h1p>
             <h1> dashboard</h1>
-            <span className="absolute top-[211px] left-[230px] rotate-45">
+            <span className="absolute top-[290px] left-[310px] rotate-45">
               !
             </span>
           </div>
-          <span className="absolute bg-secondary text-primary py-0.25 pl-1 pr-2 ml-1 mb-1 z-40 rounded-e-lg bottom-0 text-xs">
-            Next.js
-          </span>
-          <span className="absolute bg-secondary text-primary py-0.25 pl-1 pr-2 ml-1 mb-1 z-40 rounded-e-lg bottom-0 left-16 text-xs">
-            Sass
-          </span>
+          <div className="techBadges absolute bottom-1 z-40 text-primary text-xs">
+            <span className="bg-secondary/70 py-[1px] pl-1 pr-2 mb-1 ml-1 z-40 rounded-s-sm rounded-e-lg text-xs">
+              Next.js
+            </span>
+
+            <span className="bg-secondary/70 py-[1px] pl-1 pr-2 ml-1 mb-1 z-40 rounded-s-sm rounded-e-lg">
+              Sass
+            </span>
+          </div>
         </div>
       </div>
     </div>
